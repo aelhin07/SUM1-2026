d4b30c704e1318e515432ad0f54a699b32704017
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2026-05-19 13:51:01 -0400

    Create password_starter.py for password generation
    Initial implementation of a password generator with user prompts.

---
+++ sessions/session03/password/password_starter.py
@@ -0,0 +1,22 @@
+# Password Generator
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# DON'T DELETE THE ABOVE LINES!
+
+# Get number of letters, symbols and numbers from the user
+print("Welcome to my LSN0104 Password Generator!")
+# DELETE THIS LINE AND ADD YOUR CODE BELOW
+
+# Level 1 - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+# DELETE THIS LINE AND ADD YOUR CODE BELOW
+
+# Level 2 - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+# DELETE THIS LINE AND ADD YOUR CODE BELOW
+
